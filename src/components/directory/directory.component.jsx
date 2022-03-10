03669075032a2ce52d255543415b6256ad790ba5
Eduardo Rodriguez <edrodriguez@intellisys.com.do>
2022-03-10 10:48:07 -0800

    This splited up the homepage into components and did the necessary to get those components working and styled, also added the compenents and page folders.

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,57 @@
+import react from "react";
+import MenuItem from "../menu-item/menu-item.component";
+import './directory.style.scss';
+
+class Directory extends react.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            sections: [
+                {
+                    title: 'HATS',
+                    imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+                    id: 1
+                },
+                {
+                    title: 'JACKETS',
+                    imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+                    id: 2
+                },
+                {
+                    title: 'SNEAKERS',
+                    imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+                    id: 3
+                },
+                {
+                    title: 'WOMEN',
+                    imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+                    id: 4,
+                    size: 'large'
+                },
+                {
+                    title: 'MEN',
+                    imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+                    id: 5,
+                    size: 'large'
+                }
+            ]
+        }
+    }
+
+    render() {
+        return(
+            <div className="directory-menu">
+                {
+                    this.state.sections.map(({title, imageUrl, id, size}) => {
+                        return(
+                            <MenuItem key={id} title={title} imageUrl={imageUrl} size={size}/>
+                        )
+                    })
+                }
+            </div>   
+        );
+    }
+}
+
+export default Directory;
